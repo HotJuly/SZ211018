c0bf31358d3f74390b1fbafcac6f2faddbb4116a
HotJuly <949588380@qq.com>
2022-04-07 11:55:51 +0800

    27.小程序数据请求

--- 1.小程序/02.代码/硅谷云音乐/pages/index/index.js
+++ 1.小程序/02.代码/硅谷云音乐/pages/index/index.js
@@ -12,7 +12,45 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        /*
+            发送请求的三个问题
+                1.在哪发
+                    在onLoad中发送请求,因为该生命周期是最早执行的生命周期
+                    
+                2.怎么发
+                    小程序并没有遵守W3C的语法规范,所以他并不支持BOM和DOM
+                    所以此处无法使用ajax发送请求
 
+                    API:wx.request(Object object)
+
+                    注意:小程序中没有window,全局对象是wx
+
+                3.往哪发
+                    通过接口文档可以得知
+                    请求三要素:
+                        1.请求地址
+                        2.请求参数
+                        3.请求方式
+        
+        */
+
+        // console.log('window',window)
+        // console.log('wx',wx)
+
+        console.log(1)
+        wx.request({
+            url:"http://localhost:3000/banner",
+            data:{
+                type:2
+            },
+            success:()=>{
+                console.log('success')
+            },
+            fail:()=>{
+                console.log('fail')
+            }
+        })
+        console.log(2)
     },
 
     /**
