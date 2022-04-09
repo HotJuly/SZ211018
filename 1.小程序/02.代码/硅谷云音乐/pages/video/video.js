8d31fbc9a41401f5f1b6ba86e470e8e93b65489a
HotJuly <949588380@qq.com>
2022-04-09 16:02:37 +0800

    49.视频页面多video组件优化

--- 1.小程序/02.代码/硅谷云音乐/pages/video/video.js
+++ 1.小程序/02.代码/硅谷云音乐/pages/video/video.js
@@ -16,11 +16,35 @@ Page({
         videoList: [],
 
         // 用于控制scroll-view组件下拉动画的收起
-        isTrigger:false
+        isTrigger: false,
+
+        // 用于控制页面上video组件的显示隐藏
+        currentId:null
+    },
+
+    // 用于改变currentId,实现video组件和image组件之间的切换效果
+    changeCurrentId(event){
+        // console.log(event)
+        const currentId = event.currentTarget.id;
+
+        // 小程序更新数据是同步更新,更新页面是异步更新
+        // Vue中会使用$nextTick,在回调函数中去操作最新的DOM节点
+        // setData可以传入第二个实参,数据类型为函数,该函数会在视图更新之后执行
+        // 注意:如果第二次播放同一个视频,可能会出现不自动播放的效果,其实这是开发工具的bug,真机上并不存在
+        this.setData({
+            currentId
+        },()=>{
+            const videoContext = wx.createVideoContext(currentId);
+
+            videoContext.play();
+        })
+
+       
+
     },
 
     // 用于请求导航列表的数据
-    async getNavList(){
+    async getNavList() {
         const result = await myAxios('/video/group/list');
         this.setData({
             navList: result.data.slice(0, 13),
@@ -30,16 +54,16 @@ Page({
         })
     },
 
-    async handlePullDown(){
+    async handlePullDown() {
         // console.log('handlePullDown')
         await this.getVideoList();
         this.setData({
-            isTrigger:false
+            isTrigger: false
         })
     },
 
     // 该方法仅用于测试暂停视频播放API,不是本项目的功能
-    testAPI(){
+    testAPI() {
         // console.log('testAPI')
         const vid = '6079942F18A2160EF7CEC24827886C7F';
 
@@ -48,14 +72,14 @@ Page({
     },
 
     // 用于监视视频的播放操作
-    handlePlay(event){
+    handlePlay(event) {
         // console.log('handlePlay',this.oldVId)
 
         // 1.获取到当前正在播放的视频id
         const vid = event.currentTarget.id;
 
         // 判断是否具有上一个视频,而且上一个视频是否就是当前这一个视频
-        if(this.oldVId&&this.oldVId!==vid){
+        if (this.oldVId && this.oldVId !== vid) {
             const videoContext = wx.createVideoContext(this.oldVId);
             videoContext.pause();
         }
@@ -78,7 +102,7 @@ Page({
         })
 
         wx.showLoading({
-            title:"加载中,请稍等"
+            title: "加载中,请稍等"
         });
         // console.log(1)
 
@@ -92,7 +116,7 @@ Page({
     async getVideoList() {
         // 将videoList状态更新为空,可以实现页面空白的效果,优化切换效果
         this.setData({
-            videoList:[]
+            videoList: []
         })
 
         const result2 = await myAxios('/video/group', {
@@ -104,7 +128,7 @@ Page({
                 return item.data
             })
         })
-        
+
         // console.log(2)
     },
 
@@ -151,7 +175,7 @@ Page({
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh:async function () {
+    onPullDownRefresh: async function () {
         // console.log('onPullDownRefresh')
 
         // 发送请求获取最新的导航列表数据
@@ -171,7 +195,35 @@ Page({
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
-
+    onShareAppMessage: function ({
+        from,
+        target
+    }) {
+        console.log('onShareAppMessage', from, target)
+        /*
+            通过形参中的from属性,可以判断用户触发转发的渠道
+                如果from==="menu",就说明用户点击的是右上角转发按钮
+                如果from==="button",就说明用户点击的是button组件转发的
+        */
+        if (from === "menu") {
+            //    能进入这里说明用户点击了右上角转发按钮
+            return {
+                title: "硅谷云音乐",
+                imageUrl: "/static/images/dazuo.jpeg",
+                path: "/pages/index/index"
+            }
+        } else if (from === "button") {
+            //    能进入这里说明用户点击了button按钮转发
+            // 注意:自定义属性不支持大写,所有英文都会自动转为小写
+            const {
+                title,
+                imageurl
+            } = target.dataset;
+            return {
+                title,
+                imageUrl:imageurl,
+                path: "/pages/video/video"
+            }
+        }
     }
 })
