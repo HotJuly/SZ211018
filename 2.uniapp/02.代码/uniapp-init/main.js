4b9a45b3bdadbf4e956bbb4d06d4410ecf9aac2a
HotJuly <949588380@qq.com>
2022-04-12 14:44:07 +0800

    04.uniapp文件介绍

---
+++ 2.uniapp/02.代码/uniapp-init/main.js
@@ -0,0 +1,23 @@
+import App from './App'
+
+import Vue from 'vue'
+Vue.config.productionTip = false
+
+// 由于小程序中,App,Page,Component三者区分的非常清楚,而uniapp中这三者都是vue组件
+// 所以才多了这一行声明,声明当前App组件代表整个小程序
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+
+	// onLaunch: function() {
+	// 	console.log('App Launch')
+	// },
+	// onShow: function() {
+	// 	console.log('App Show')
+	// },
+	// onHide: function() {
+	// 	console.log('App Hide')
+	// }
+})
+app.$mount()
