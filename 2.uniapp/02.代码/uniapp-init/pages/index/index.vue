907fb475a554df89b9dd2c0857c03e8a402b2af1
HotJuly <949588380@qq.com>
2022-04-12 15:59:47 +0800

    07.HBuilder插件安装

--- 2.uniapp/02.代码/uniapp-init/pages/index/index.vue
+++ 2.uniapp/02.代码/uniapp-init/pages/index/index.vue
@@ -1,52 +1,68 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="indexContainer">
+		<view class="header">
+			<image class="logo" src="/static/images/logo.png" mode=""></image>
+			<view class="search">
+				<view class="iconfont icon-sousuo"></view>
+				<input 
+				class="searchInput" 
+				type="text" 
+				placeholder="搜索商品" 
+				placeholder-class="placeholder"
+				value="" />
+			</view>
+			<button class="username">七月</button>
 		</view>
 	</view>
+	<!-- <div>
+		123
+	</div> -->
+	<!-- uniapp兼容小程序的组件,也支持html的标签,推荐大家使用小程序组件 -->
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="stylus">
+.indexContainer
+	.header
+		display flex
+		align-items center
+		padding-top 20upx
+		.logo
+			width 118upx
+			height 40upx
+			margin 0 20upx
+		.search
+			position relative
+			height 60upx
+			background-color #ccc
+			border-radius 10upx
+			padding-left 60rpx
+			flex-grow 1
+			.iconfont
+				position absolute
+				top 50%
+				transform translateY(-50%)
+				left 20rpx
+			.searchInput
+				height 100%
+				.placeholder
+					font-size 24rpx
+					text-align center
+					text-indent -60rpx
+		.username
+			width 140upx
+			height 60upx
+			font-size 24upx
+			margin 0 20upx
 </style>
