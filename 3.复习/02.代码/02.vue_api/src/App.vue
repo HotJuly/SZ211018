7e05d050572f18e59beb8380ca13f2c706525800
HotJuly <949588380@qq.com>
2022-04-26 15:54:01 +0800

    23.Vue-API(全局配置)

--- 3.复习/02.代码/02.vue_api/src/App.vue
+++ 3.复习/02.代码/02.vue_api/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -12,7 +11,8 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  a:9
 }
 </script>
 
